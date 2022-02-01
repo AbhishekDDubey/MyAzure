8010c82acad0ff59389cebe0f1d5447c274ec022
AbhishekDDubey <98838850+AbhishekDDubey@users.noreply.github.com>
2022-02-01 23:38:36 +0530

    Create RunDeployment.ps1
    Power shell script to for ARM template depoyment

---
+++ RunDeployment.ps1
@@ -0,0 +1,5 @@
+# Connect-AzAccount
+
+$rg='storage-rg2'
+New-AzResourceGroup -Name $rg -Location "central india" -Force
+New-AzResourceGroupDeployment -Name "deployment1" -ResourceGroupName $rg -TemplateFile "test-dep2.json"
